3b08db478813d9e502e93df83f9d494b173a7b54
giovanaalvarengasouza-boop <giovana.alvarenga.souza@escola.pr.gov.br>
2026-09-14 11:41:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const botoes= document .querySelectorAll("button"); 
 
+botoes.forEach(fuction(botao) { 
+        let curtiu = false;
+        botao.addEventlistener(click", botaoclicado);
+        function botaaoClicado() {
+          console.log ("fui criado');
+          let texto= botao. querySelector("span"); 
+            if (curtiu=== false ) {
+            texto.textContent++;
+            curtiu = true; 
+            } else{ 
+            texto.textContent--;
+            curtiu = false; 
+            }
+        } 
+}) 
